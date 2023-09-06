64d3e122fdcb698acffd765ae70fe4d15691ab2c
andrebovis <andre_bovis@hotmail.com>
2023-09-06 01:51:06 -0300

    parte 1

--- src/main/java/com/andrebovis/course/entities/User.java
+++ src/main/java/com/andrebovis/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.andrebovis.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ private static final long serialVersionUID = 1L;
 	private String  email ;
 	private String phone;
 	private String password;
+	@OneToMany(mappedBy ="client" )
+	private List<Order> orders = new ArrayList<>();
+	
  public User() {
 	 }
  
@@ -68,7 +74,9 @@ public void setPassword(String password) {
 	this.password = password;
 }
 
-
+public List<Order> getOrders() {
+	return orders;
+}
 
 @Override
 public int hashCode() {
@@ -97,5 +105,9 @@ public String toString() {
 			+ "]";
 }
 
+
+
+
+ 
 
 }
