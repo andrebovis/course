1e0211b0682eee50424d7fb6c61df5219a5075a4
andrebovis <andre_bovis@hotmail.com>
2023-09-13 02:49:52 -0300

    userinsert

--- src/main/java/com/andrebovis/course/services/UserService.java
+++ src/main/java/com/andrebovis/course/services/UserService.java
@@ -21,6 +21,11 @@ public class UserService {
 
 	public User findById(Long id) {
 	Optional<User> obj = repository.findById(id);
-	return obj.get();
+	return obj.get();	
+
+	}
+	
+	public User  inster(User obj) {
+		return repository.save(obj); 
 	}
 }
