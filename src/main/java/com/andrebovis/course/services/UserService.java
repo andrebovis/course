2fa3db41e4117b453746cd0b09b7867ddb3109e8
andrebovis <andre_bovis@hotmail.com>
2023-09-19 02:35:20 -0300

    databaseexception

--- src/main/java/com/andrebovis/course/services/UserService.java
+++ src/main/java/com/andrebovis/course/services/UserService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.ResourceAccessException;
 
 import com.andrebovis.course.entities.User;
 import com.andrebovis.course.repositories.UserRepository;
+import com.andrebovis.course.services.exceptions.DatabaseException;
 import com.andrebovis.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		}	catch (EmptyResultDataAccessException e ) {
+		 throw new  ResourceNotFoundException (id);
+		} catch  (DataIntegrityViolationException e ) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	public User  update(Long id,  User obj) {
 		User entity = repository.getReferenceById(id);
